348be3a45d5cebe47edea28231d65271198f209d
trudie anderson <trudiekennedy@gmail.com>
2025-05-22 15:38:12 +0100

    Set up of app.py & test for GET route

--- app.js
+++ app.js
@@ -6,4 +6,30 @@ app.use(express.json())
 
 app.use(cors());
 
+const gigs = [{
+  name: "Ane Brun",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ane_Brun_2012-03-29_003.jpg/500px-Ane_Brun_2012-03-29_003.jpg",
+  description: "Folk singer-songwriter",
+  date: "2025/06/01",
+  location: "The Town Hall, Accrington",
+  id: 1,
+},
+{
+  name: "Radiohead",
+  image: "https://cdn.britannica.com/98/162198-050-6452139D/Radiohead-business-models-British-performers-innovator-Internet-2012.jpg?w=300",
+  description: "Alternative",
+  date: "2025/06/14",
+  location: "The O2, Bristol",
+  id: 2,
+},
+{
+  name: "LCD Soundsystem",
+  image: "https://magazine-resources.tidal.com/uploads/2017/09/LCD_1200.jpg",
+  description: "Art-rock, electronic experimentalism, proto-punk fusion",
+  date: "2025/07/12",
+  location: "Millenium Stadium, Cardiff",
+  id: 3,
+}];
+
+
 module.exports = app;
