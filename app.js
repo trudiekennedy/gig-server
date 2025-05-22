7908bd8296448da861ae4905702f0734c264f99e
trudie anderson <trudiekennedy@gmail.com>
2025-05-22 16:06:49 +0100

    Tests and app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const gigs = [{
   name: "Ane Brun",
   image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ane_Brun_2012-03-29_003.jpg/500px-Ane_Brun_2012-03-29_003.jpg",
   description: "Folk singer-songwriter",
-  date: "2025/06/01",
+  date: new Date("2025-06-01T00:00:00.000Z"),
   location: "The Town Hall, Accrington",
   id: 1,
 },
@@ -19,7 +19,7 @@ const gigs = [{
   name: "Radiohead",
   image: "https://cdn.britannica.com/98/162198-050-6452139D/Radiohead-business-models-British-performers-innovator-Internet-2012.jpg?w=300",
   description: "Alternative",
-  date: "2025/06/14",
+  date: new Date("2025-06-14T00:00:00.000Z"),
   location: "The O2, Bristol",
   id: 2,
 },
@@ -27,13 +27,15 @@ const gigs = [{
   name: "LCD Soundsystem",
   image: "https://magazine-resources.tidal.com/uploads/2017/09/LCD_1200.jpg",
   description: "Art-rock, electronic experimentalism, proto-punk fusion",
-  date: "2025/07/12",
+  date: new Date("2025-07-12T00:00:00.000Z"),
   location: "Millenium Stadium, Cardiff",
   id: 3,
 }];
 
 // Routes
-
+app.get('/gigs', (req, res) => {
+  res.send(gigs);
+});
 
 
 // Exports
